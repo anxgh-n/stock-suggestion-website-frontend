d563d2248bcfd5ed2970dbb9fc4da41cf4a7145f
Anjali Vijayakumar Meena(UST <288042@ust.com>
2024-11-12 09:22:08 +0530

    Handled login and signup

--- Loginpage/src/components/NavigationComponent.jsx
+++ Loginpage/src/components/NavigationComponent.jsx
@@ -1,4 +1,15 @@
+import { useNavigate } from "react-router";
 export default function NavigationComponent(){
+  let navigate = useNavigate();
+
+  function handleLogin(){
+    navigate("/login");
+  }
+
+  function handleSignUp(){
+    navigate("/signup");
+  }
+
     return (<>
       {/* Navigation Bar */}
       <nav className="absolute top-0 left-0 w-full py-4 bg-transparent z-10">
@@ -20,10 +31,14 @@ export default function NavigationComponent(){
             </a>
           </div>
           <div className="flex space-x-4">
-            <button className="text-black-600 rounded-full px-4 py-2">
+            <button className="text-black-600 rounded-full px-4 py-2"
+            onClick={handleLogin}
+            >
               Login
             </button>
-            <button className="bg-blue-900 text-white rounded-full px-4 py-2">
+            <button className="bg-blue-900 text-white rounded-full px-4 py-2"
+            onClick={handleSignUp}
+            >
               Sign Up
             </button>
           </div>
